fcff470a300c594768e25872531945a8c3b78ef0
Wari <waripamooweidudafa@gmail.com>
2025-01-08 19:03:26 +0000

    Update experience date format and remove outdated entries

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -7,28 +7,8 @@ export interface Experience {
 export const experiences: Experience[] = [
   {
     title: "Incoming Software Engineer Intern",
-    date: "2025",
+    date: "Jun 2025",
     description: "",
-    technologies: [""],
-  },
-  {
-    title: "Web Developer",
-    date: "2024",
-    description:
-      "I worked at HackSussex, making sure the website was pleasing to look at and easy to use.",
-    technologies: ["React", "TypeScript", "Bootstrap"],
-  },
-  {
-    title: "Maths Tutor",
-    date: "2023",
-    description:
-      "I helped GCSE students with Mathematics concepts they were struggling with.",
-    technologies: [""],
-  },
-  {
-    title: "Computer Science Tutor",
-    date: "2024",
-    description: "Helped A-Level CS students go over various topics.",
     technologies: [""],
   },
 ];
