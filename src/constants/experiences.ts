e8ad1cc172c4ebf3b94c2a7c84b87580bb1337d1
Wari <waripamooweidudafa@gmail.com>
2025-07-11 23:54:36 +0100

    Update title for Software Engineering Placement Intern to Software Engineering Intern

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -7,7 +7,7 @@ export interface Experience {
 
 export const experiences: Experience[] = [
   {
-    title: "Software Engineering Placement Intern",
+    title: "Software Engineering Intern",
     date: "Jun 2025 - Present",
     description:
       "Working as a software engineer intern at a top financial firm",
