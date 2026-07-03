d2d9022a04fc1a595694ae9682f678ab5c8e86d9
Waripamo-owei Dudafa <60980271+wdudafa@users.noreply.github.com>
2026-07-03 18:35:44 +0100

    Created different files for all the different sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import AboutSection from "./components/about-section";
+import ContactSection from "./components/contact-section";
+import ExperienceSection from "./components/experience-section";
+import HeroSection from "./components/hero-section";
+import ProjectsSection from "./components/projects-section";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <ContactSection />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/hero-section.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div
+      style={{
+        backgroundColor: "darkslategrey",
+        padding: 20,
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 35,
+          fontWeight: "bold",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        wdudafa.com
+      </h1>
+      <p
+        style={{
+          fontSize: 25,
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        Software Engineer
+      </p>
+      <p
+        style={{
+          fontSize: 15,
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        *Scroll down*
+      </p>
+    </div>
+  );
+}
